529dbbccd3da0aeb352a28f543840ddc1d46d87b
David T. <63472790+DavidT-sudo@users.noreply.github.com>
2025-11-20 00:29:16 +0200

    fix: remove display block setting from navbar logo

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -43,7 +43,7 @@ function NavBar() {
       onToggle={next => setExpanded(next)}
       className={scrolled ? 'scrolled' : ''}
     >
-      <Container className="d-flex justify-content-center d-lg-block">
+      <Container>
         <Navbar.Brand href="#home" className="flex">
           <Image
             src={logo}
